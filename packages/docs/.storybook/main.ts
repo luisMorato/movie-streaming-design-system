d8ab1d9014cb84349533ae98850151450c302055
Luis Fernando Morato <luisfernandomorato_170701@outlook.com>
2024-10-10 12:17:56 -0300

    7º Commit - Fix: Error when open storybook on GH pages

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,34 +1,39 @@
-import type { StorybookConfig } from "@storybook/react-vite"
+import type { StorybookConfig } from '@storybook/react-vite'
 
-import { join, dirname } from "path"
+import { join, dirname } from 'path'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")))
+  return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
-  viteFinal: (config, { configType }) => {
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  viteFinal: async (config, { configType }) => {
+    const { mergeConfig } = await import('vite')
+
     if (configType === 'PRODUCTION') {
       config.base = '/movie-streaming-design-system/'
     }
 
-    return config
+    return mergeConfig(config, {})
   },
 }
 
--- packages/docs/src/components/Colors-grid.tsx
+++ packages/docs/src/components/Colors-grid.tsx
@@ -5,21 +5,18 @@ import { colors } from '@movie-streaming-ui/tokens'
 export const ColorsGrid = () => {
   return Object.entries(colors).map(([key, color]) => {
     return (
-        <div 
-            key={key} 
-            style={{ backgroundColor: color, padding: '2rem' }}
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF',
+          }}
         >
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF',
-                }}
-            >
-                <strong>${key}</strong>
-                <span>{color}</span>
-            </div>
+          <strong>${key}</strong>
+          <span>{color}</span>
         </div>
+      </div>
     )
   })
 }
